d57627aafd798b9c4f11aee58dc8900e348a2a50
Atilla.G <atillagoguslu@hotmail.com>
2025-03-27 00:15:37 +0300

    Implement contact retrieval endpoints and update response structure in server.js; refactor contact service functions for better error handling.

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import { pinoHttp } from 'pino-http';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 dotenv.config();
 
@@ -12,28 +13,57 @@ const setupServer = () => {
   app.use(express.json());
   app.use(pinoHttp());
 
-
   // All Routes below are for testing purposes
   app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send({
+      status: 200,
+      message: 'Hello World',
+      data: {},
+    });
   });
 
   app.get('/contacts', (req, res) => {
-    res.send('Hello World');
+    try {
+      const contacts = getAllContacts();
+      res.status(200).send({
+        status: 200,
+        message: 'Contacts fetched successfully',
+        data: contacts,
+      });
+    } catch (error) {
+      res.status(404).send({
+        status: 404,
+        message: 'Contacts not found',
+        error: error.message,
+      });
+    }
   });
 
-
-
-
-
-
-
-
+  app.get('/contacts/:id', (req, res) => {
+    try {
+      const contact = getContactById(req.params.id);
+      res.status(200).send({
+        status: 200,
+        message: 'Contact fetched successfully',
+        data: contact,
+      });
+    } catch (error) {
+      res.status(404).send({
+        status: 404,
+        message: 'Contact not found',
+        error: error.message,
+      });
+    }
+  });
 
   app.use('*', (req, res) => {
-    res.status(404).send({ message: 'Not found' });
+    res.status(404).send({
+      status: 404,
+      message: 'Not found',
+      data: {},
+    });
   });
-  
+
   app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
   });
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,21 @@
 import Contact_Model from '../db/models/contact.js';
 
-const getAllContacts = async (req, res) => {
-  const contacts = await Contact_Model.find();
-  res.status(200).json(contacts);
+const getAllContacts = async () => {
+  try {
+    const contacts = await Contact_Model.find();
+    return contacts;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
-export { getAllContacts };
+const getContactById = async (id) => {
+  try {
+    const contact = await Contact_Model.findById(id);
+    return contact;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export { getAllContacts, getContactById };
