146213a5d7b45840787ff44ad9c55beeba5c9078
Atilla.G <atillagoguslu@hotmail.com>
2025-04-03 16:53:31 +0300

    Refactor server.js to store PORT in a constant for improved readability and maintainability; update server listening logic accordingly.

--- src/server.js
+++ src/server.js
@@ -6,23 +6,23 @@ import contactsRouter from './routers/contacts.js';
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 
+const PORT = process.env.PORT;
+
 const setupServer = () => {
   const app = express();
   app.use(cors());
   app.use(express.json());
   app.use(pinoHttp());
-  // --------------------------------------- All Routes are below
+  // ----------------------------------- All Routes are below ---
   app.use('/', rootRouter);
   app.use('/contacts', contactsRouter);
   // ------------------------------------------------------------
-
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
-  app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
-
   return app;
 };
 
