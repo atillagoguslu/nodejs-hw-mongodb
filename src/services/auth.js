a45f0d9afdfd030c6820c6c712b085259612dd4e
Atilla.G <atillagoguslu@hotmail.com>
2025-05-05 18:11:19 +0300

    Refactor authentication controllers and services to enhance response structure and logging. Updated routes to use controller functions directly.

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,16 +1,19 @@
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
 import createHttpError from 'http-errors';
+import User from '../db/models/auth.js';
 
 const registerService = async (name, email, password) => {
-
+  console.log('In Register Service: Name:', name);
+  console.log('In Register Service: Email:', email);
+  console.log('In Register Service: Password:', password);
   const user = await User.findOne({ email });
   if (user) {
     throw createHttpError(409, 'Email in use');
   }
-
   const hashedPassword = await bcrypt.hash(password, 10);
+  console.log('In Register Service: Hashed Password:', hashedPassword);
   const newUser = await User.create({ name, email, password: hashedPassword });
+  console.log('In Register Service: New User:', newUser);
   return newUser;
 };
 
@@ -24,6 +27,6 @@ const loginService = async (email, password) => {
     throw createHttpError(401, 'Invalid credentials');
   }
   return user;
-}; 
+};
 
 export { registerService, loginService };
